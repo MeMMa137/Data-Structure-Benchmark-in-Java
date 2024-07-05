8ec1af4066b6d6cd4d6aa6a0312f18432f704b24
Memma <132008224+MeMMa137@users.noreply.github.com>
2024-07-05 11:31:50 +0200

    Add files via upload

---
+++ Data_Benchmark/src/data_benchmark/ArrayBenchmark.java
@@ -0,0 +1,13 @@
+package data_benchmark;
+
+import static data_benckmark.Data_Benchmark.SOP;
+
+class ArrayBenchmark implements Data_Benchmark {
+
+    private int v[]; //array di partenza per mettere tutte le implmentazioni sullo stesso piano
+
+    ArrayBenchmark() {
+        v = new int[0]; //inizializza vuoto
+    }
+    
+}
