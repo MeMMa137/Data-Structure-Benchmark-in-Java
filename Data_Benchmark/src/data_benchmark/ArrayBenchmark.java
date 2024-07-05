28f14faba41dbd469dff3c844349a16063cf62bf
Memma <132008224+MeMMa137@users.noreply.github.com>
2024-07-05 13:32:05 +0200

    Add files via upload

--- Data_Benchmark/src/data_benchmark/ArrayBenchmark.java
+++ Data_Benchmark/src/data_benchmark/ArrayBenchmark.java
@@ -9,5 +9,54 @@ class ArrayBenchmark implements Data_Benchmark {
     ArrayBenchmark() {
         v = new int[0]; //inizializza vuoto
     }
+    @Override
+    public long eseguiSomma() {
+        long ti = System.currentTimeMillis();
+        SOP("VALORE somma = " + somma());
+        return System.currentTimeMillis() - ti;
+    }
+
+    @Override
+    public long eseguiSommaIndietro() {
+        long ti = System.currentTimeMillis();
+        SOP("VALORE somma = " + sommaIndietro());
+        return System.currentTimeMillis() - ti;
+    }
+
+    @Override
+    public long eseguiInsTesta(int[] v) {
+        long ti = System.currentTimeMillis();
+        for (int i = 0; i < v.length; i++) {
+            insTesta(v[i]);
+        }
+        return System.currentTimeMillis() - ti;
+    }
+
+    @Override
+    public long eseguiInsCoda(int[] v) {
+        long ti = System.currentTimeMillis();
+        for (int i = 0; i < v.length; i++) {
+            insCoda(v[i]);
+        }
+        return System.currentTimeMillis() - ti;
+    }
+
+    @Override
+    public long eseguiInsInPos(int[] v, int[] p) {
+        long ti = System.currentTimeMillis();
+        for (int i = 0; i < v.length; i++) {
+            insInPos(v[i], p[i]);
+        }
+        return System.currentTimeMillis() - ti;
+    }
+
+    @Override
+    public long eseguiCancInPos(int[] p) {
+        long ti = System.currentTimeMillis();
+        for (int i = 0; i < p.length; i++) {
+            cancInPos(p[i]);
+        }
+        return System.currentTimeMillis() - ti;
+    }
     
 }
