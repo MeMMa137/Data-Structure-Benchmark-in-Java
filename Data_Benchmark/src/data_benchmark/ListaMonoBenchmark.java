ca6a22fd2e92001c3a390f3d49350c1aceed21fa
Memma <132008224+MeMMa137@users.noreply.github.com>
2024-07-06 21:47:33 +0200

    new functions for mono list

--- Data_Benchmark/src/data_benchmark/ListaMonoBenchmark.java
+++ Data_Benchmark/src/data_benchmark/ListaMonoBenchmark.java
@@ -50,4 +50,56 @@ public class ListaMonoBenchmark extends Benchmark {
         return System.currentTimeMillis() - ti;
     }
     
+     @Override
+    public long eseguiCancInPos(int[] p) {
+        long ti = System.currentTimeMillis();
+        for (int i = 0; i < p.length; i++) {
+            cancellaInPos(p[i]);
+        }
+        return System.currentTimeMillis() - ti;
+    }
+    
+    //FUNZIONI SINGOLE
+
+    int somma() {
+        int s = 0;
+        NodoMono p = h;
+        while (p != null) {
+            s = s + p.inf;
+            p = p.next;
+        }
+        return s;
+    }
+
+    int sommaIndietro() {
+        NodoMono copiaInvertita = inverti();
+        int s = 0;
+        while (copiaInvertita != null) {
+            s += copiaInvertita.inf;
+            copiaInvertita = copiaInvertita.next;
+        }
+        return s;
+    }
+
+    NodoMono inverti() {
+        NodoMono inverti = null, a = h, p;
+        while (a != null) {
+            p = a.next;
+            a.next = inverti;
+            inverti = a;
+            a = p;
+        }
+        return inverti;
+    }
+
+    void insInTesta(int val) {
+        NodoMono n = new NodoMono(val);
+        n.next = h; //il next del nuovo elemento punterà all'elemento che sta puntando la testa (non ad h stesso), essendo che n dovrà diventare il suo precendente
+        if (t == null) //se non c'è una coda (ovvero non ci sono elementi)
+        {
+            t = n; //il nuovo elemento diventa la coda
+        }
+        h = n; //il nuovo elemento diventerà la testa 
+    }
+    
 }
