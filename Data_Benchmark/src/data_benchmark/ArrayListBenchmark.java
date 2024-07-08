fa1dcc8e916f25581996dd99e2f0dc467fd7d0cf
Memma <132008224+MeMMa137@users.noreply.github.com>
2024-07-08 12:39:39 +0200

    ArrayList structure finished

--- Data_Benchmark/src/data_benchmark/ArrayListBenchmark.java
+++ Data_Benchmark/src/data_benchmark/ArrayListBenchmark.java
@@ -44,4 +44,44 @@ public class ArrayListBenchmark extends Benchmark {
         return System.currentTimeMillis() - ti;
     }
     
+    @Override
+    public long eseguiInsInPos(int[] v, int[] p) {
+        long ti = System.currentTimeMillis();
+        for (int i = 0; i < v.length; i++) {
+            arrayList.add(p[i], v[i]); //pos, val
+        }
+        return System.currentTimeMillis() - ti;
+    }
+
+    @Override
+    public long eseguiCancInPos(int[] p) {
+        long ti = System.currentTimeMillis();
+        for (int i = 0; i < p.length; i++) {
+            arrayList.remove(p[i]);
+        }
+        return System.currentTimeMillis() - ti;
+    }
+    
+    //FUNZIONI SINGOLE
+
+    int somma() {
+        int s = 0;
+        for (int i = 0; i < arrayList.size(); i++) {
+            s += arrayList.get(i);
+        }
+        return s;
+    }
+
+    int sommaIndietro() {
+        int s = 0;
+        for (int i = arrayList.size() - 1; i >= 0; i--) {
+            s += arrayList.get(i);
+        }
+        return s;
+    }
+
 }
+    
+}
+
+
